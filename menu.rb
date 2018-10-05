5da7fb31b603e3336ae242b26515398b3f242998
Spencer King <spencerking@users.noreply.github.com>
2018-10-04 22:21:27 -0500

    Build out install functionality in menu.rb

--- menu.rb
+++ menu.rb
@@ -1,9 +1,11 @@
 require 'ncurses'
+require './helpers.rb'
 
 def menu()
     scr = Ncurses.initscr
     Ncurses.cbreak
     Ncurses.noecho
+
     Ncurses.keypad(scr, true)
 
     # Initial menu
@@ -28,12 +30,33 @@ def menu()
             menu.menu_driver(Ncurses::Menu::REQ_DOWN_ITEM)
         elsif 10
 
-        	# Top level menu 
-        	if menu.current_item.item_name == "Launch"
+        	# Top level menu
+            if menu.current_item.item_name == "Install"
+                system "clear"
+                level = "install"
+                # TODO Modularize this
+                dir = ENV['HOME'] + "/.bacchus/presets/*"
+                files = Dir[dir]
+                choices = []
+                files.each do |file|
+                    if !File.directory? file
+                        file_name = File.basename(file, ".*")
+                        if file_name != "README"
+                        	choices.push(file_name)
+                        end
+                    end
+                end
+                choices.push("Exit")
+
+                menu = build_menu(choices)
+                # TODO: set_format should account for terminal size to determine how many rows to display at once
+                #menu.set_format(25,1)
+                menu.post_menu
+
+        	elsif menu.current_item.item_name == "Launch"
         		system "clear"
         		level = "launch"
         		# TODO Modularize this
-        		# Very similar to what we do for -a option
         		dir = ENV['HOME'] + "/.bacchus/prefixes/*"
 			    files = Dir[dir]
 			    choices = []
@@ -50,6 +73,18 @@ def menu()
 
     		elsif menu.current_item.item_name == "Exit"
     			exit
+
+            # Install menu
+            elsif level == "install"
+                # TODO turn this into a function
+                    # Copied from bacchus.rb
+                recipe = parse(menu.current_item.item_name)
+                build_prefix(recipe)
+                if recipe['winetricks'] != ""
+                    winetricks(recipe)
+                end
+                install(recipe)
+                build_launcher(recipe)
 
     		# Launch menu
     		elsif level == "launch"
